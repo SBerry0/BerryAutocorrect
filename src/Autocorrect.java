929095207199f6e6a39257e3d6e02c7a4c1b6a43
Sohum Berry <sohumberry@gmail.com>
2025-03-23 13:27:35 -0700

    Eighth Log

--- src/Autocorrect.java
+++ src/Autocorrect.java
@@ -33,9 +33,11 @@ public class Autocorrect {
      * to threshold, sorted by edit distance, then sorted alphabetically.
      */
     public String[] runTest(String typed) {
+        // Use hashmap to quickly figure out if the word is already in the dictionary
         if (dictHash.contains(typed)) {
             return new String[0];
         }
+        // Marginally narrow down the number of words to search through by restricting the lengths we look at
         ArrayList<String> candidates = new ArrayList<>();
         int stringLen = typed.length();
         for (String s : dictionary) {
@@ -44,37 +46,47 @@ public class Autocorrect {
             }
         }
 
+        // Adds each of the candidate words into a sorted position based on its edit distance
+        // Holds the indices of where each edit distance starts within the ArrayList
         int[] positions = new int[threshold];
         for (int i = 0; i < threshold; i++) {
             positions[i] = 0;
         }
+        // The output ArrayList
         ArrayList<String> options = new ArrayList<>();
+        // For each candidate, calculate its edit distance
         for (String s : candidates) {
             int editDistance = calcDistance(typed, s);
+            // If the edit distance is within the threshold, add it to its edit distance's position in the arrayList
             if (editDistance <= positions.length) {
                 options.add(positions[editDistance-1], s);
+                // Increment the indices of all larger edit distances to account for the new item that was inserted
                 for (int i = editDistance-1; i < positions.length; i++) {
                     positions[i]++;
                 }
             }
         }
 
+        // If no words could be found, return an array with a word in it to differentiate from perfectly spelled word
         if (options.isEmpty()) {
+            // Intentionally misspell the word so it cannot be mistaken for coming from the dictionary
             return new String[] {"invld"};
         }
 
+        // If there are possible words, convert ArrayList into an Array and return it
         String[] out = new String[options.size()];
         for (int i = 0; i < options.size(); i++) {
             out[i] = options.get(i);
         }
-
         return out;
     }
 
-
+    // Calculates the number of edits that need to be made to a word to match a goal word
     public int calcDistance(String s, String goal) {
+        // Create the tabulation board with a buffer
         int[][] board = new int[s.length()+1][goal.length()+1];
 
+        // Fill the buffers with the corresponding lengths if one word's length it 0
         for (int i = 0; i < s.length()+1; i++) {
             board[i][0] = i;
         }
@@ -82,18 +94,22 @@ public class Autocorrect {
             board[0][i] = i;
         }
 
+        // Iterate through the board
         for (int i = 1; i < s.length()+1; i++) {
             for (int j = 1; j < goal.length()+1; j++) {
+                // If the characters are the same, place the edit distance of the tails of each into that square
                 if (s.charAt(i-1) == goal.charAt(j-1)) {
                     board[i][j] = board[i-1][j-1];
                 }
                 else {
+                    // Otherwise add one to the minimum of the three possibilities
                     board[i][j] = 1 + Math.min(board[i-1][j],
                                       Math.min(board[i][j-1],
                                                 board[i-1][j-1]));
                 }
             }
         }
+        // Return the last square in the board to get the edit distance
         return board[s.length()][goal.length()];
     }
 
@@ -125,26 +141,39 @@ public class Autocorrect {
     }
 
     public static void main(String[] args) {
+        // Load in the dictionary
         String[] dict = loadDictionary("large");
 
+        // Initialize the Scanner and the Autocorrect class
         Scanner s = new Scanner(System.in);
         Autocorrect a = new Autocorrect(dict, 3);
 
+        // Inside a while loop that will keep the program running
         while (true) {
+            // Request and read in a word
             System.out.println("Please enter a word:");
             String input = s.nextLine();
+
+            // Break out of the loop to end the program if the user types 'q'
             if (input.equals("q")) {
+                System.out.println("Bye!");
                 break;
             }
+
+            // Get the possible spellings of the  misspelled word from the Autocorrect class
             String[] results = a.runTest(input);
+            // If it's empty, the word is in the dictionary
             if (results.length == 0) {
                 System.out.println(input + " is spelled correctly");
             } else if (results[0].equals("invld")) {
+                // If the first item in the results is a misspelled word, there were no matched found
                 System.out.println("No matches found.");
             } else {
+                // Otherwise, provide all the possible words to the given threshold with the proper formatting
                 System.out.println("Did you mean:");
                 for (int i = 0; i < results.length; i++) {
                     System.out.print(results[i]);
+
                     if (i < results.length-1) {
                         System.out.print(", ");
                     } else {
